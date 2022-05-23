6eb2db3ad76669a0fc64f78961b939f1470894f3
BIRJU-BHARTI <104707546+BIRJU-BHARTI@users.noreply.github.com>
2022-05-23 13:11:13 +0530

    Add files via upload

---
+++ hello-world.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello-world!"<<endl; 
+    /* 1. cout is a keywords which helps to print the element which is " "
+     2. << is an insertion operator.
+     3. endl is used for new line.
+    */
+    return 0;
+}
